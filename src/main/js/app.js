7a705480d0d18941eec0b16da91286e162cc619d
Aleksei Samoilov <alekseysamoylov89@gmail.com>
2018-05-23 09:13:32 +0300

    redux start part 1

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,16 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 import UIRouter from './router/UIRouter';
+import {combineReducers, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import productsReducer from './redux/reducers/products-reducer'
+import userReducer from './redux/reducers/user-reducer'
 
 const browserHistory = Router.browserHistory;
+
+const allReducers = combineReducers({
+    products: productsReducer,
+    user: userReducer
+});
+
+const store = createStore(
+    allReducers,
+    {
+        products: [{name: 'iPhone'}],
+        user: 'Michael'
+    },
+    window.devToolsExtension && window.devToolsExtension()
+);
+
+console.log(store.getState());
+
+const updateUserAction = {
+    type: 'updateUser',
+    payload: {
+        user: 'John'
+    }
+};
+
+store.dispatch(updateUserAction);
 
 
 function loadApplication() {
 
     ReactDOM.hydrate(
-        <Router history={browserHistory}>
-            <UIRouter/>
-        </Router>,
+        <Provider store={store}>
+            <Router history={browserHistory}>
+                <UIRouter/>
+            </Router>
+        </Provider>,
         document.getElementById('react'));
 
 }
